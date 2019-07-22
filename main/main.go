1e969a2127a3780f508905a93ed36ed2377b1d5b
Nikita Leskin <lyeskin@gmail.com>
2019-07-22 21:30:10 +0300

    Initial commit

---
+++ main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
